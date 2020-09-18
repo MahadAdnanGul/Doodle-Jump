5674b5d420cf147e3a1bedee7853a8a465445415
MahadAdnanGul <64431074+MahadAdnanGul@users.noreply.github.com>
2020-09-18 16:35:20 +0500

    Game

---
+++ Assets/Scripts/Scene_change.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scene_change : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+   public void play_scene()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void stop_scene()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
---
+++ Assets/Scripts/doodled.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class doodled : MonoBehaviour
+{
+    public Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag=="Player")
+        {
+            anim.SetBool("doodled", true);
+        }
+    }
+}
